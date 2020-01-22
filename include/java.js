2d69c05bf39369bfe43c081b6c71daac43a55967
Beeclust <beeclustmrsl@gmail.com>
2020-01-22 22:19:48 +0530

    first commit

---
+++ include/java.js
@@ -0,0 +1,96 @@
+var slideIndex = 0;
+var timer;
+var i;
+var slides = document.getElementsByClassName("slide");
+var dot = document.getElementsByClassName("dot");
+var read = document.querySelectorAll(".about-project-con");
+const domain = document.querySelectorAll(".domain");
+let currentDomain = 0;
+slideshow();
+
+{
+	for(var i=0;i<read.length;i++)
+		read[i].style.display = "none";
+
+	for(i=0;i<domain.length;i++)
+		domain[i].style.display = "none";
+	
+	domain[currentDomain].style.display = "block";
+
+	document.querySelector(".nav-links-con-reponsive").style.display = "none";
+}
+
+document.querySelector(".next").addEventListener("click" , () =>{
+	domain[currentDomain].style.display = "none";
+	if(currentDomain === domain.length-1)
+		currentDomain = 0;
+	else 
+		currentDomain++;
+	domain[currentDomain].style.display = "block";
+})
+
+document.querySelector(".prev").addEventListener("click" , () =>{
+	domain[currentDomain].style.display = "none";
+	if(currentDomain === 0)
+		currentDomain = domain.length-1;
+	else 
+		currentDomain--;
+	domain[currentDomain].style.display = "block";
+})
+
+function currentSlide(n)
+{
+	clearTimeout(timer);
+	for (i=0;i<slides.length;i++)
+		slides[i].style.display = "none";
+	for(i=0;i<dot.length;i++)
+		dot[i].className = dot[i].className.replace("active"," ");
+	slides[n-1].style.display = "flex";
+	dot[n-1].className += " active";
+	slideIndex=n;
+	timer =setTimeout(slideshow,10000);
+}
+
+function slideshow()
+{ 
+    clearTimeout(timer);
+    for (i=0;i<slides.length;i++)
+		slides[i].style.display = "none";
+	for(i=0;i<dot.length;i++)
+		dot[i].className = dot[i].className.replace("active"," ");
+	slideIndex++;
+	if (slideIndex > slides.length)
+		slideIndex=1;
+	slides[slideIndex-1].style.display = "flex";
+	dot[slideIndex-1].className += " active";
+	timer = setTimeout(slideshow,10000);	
+}
+
+function Function(n)
+{
+	if(read[n].style.display === "none")
+		read[n].style.display = "block";
+	else
+		read[n].style.display = "none";
+}
+
+
+document.querySelector(".member-btn").addEventListener("click" , function(){
+	if(document.querySelector(".team-member-con").style.display == "block")
+		document.querySelector(".team-member-con").style.display = "none";
+	else
+		document.querySelector(".team-member-con").style.display = "block";
+		
+})
+
+
+document.querySelector(".hamburger-con").addEventListener("click" , function(){
+	if(document.querySelector(".nav-links-con-reponsive").style.display === "none")
+		document.querySelector(".nav-links-con-reponsive").style.display = "block";
+	else
+	document.querySelector(".nav-links-con-reponsive").style.display = "none";
+})
+
+window.addEventListener("load" , () =>{
+	document.querySelector(".preload").classList.add("preload_remove");
+})
